e1d67ccc9caf75ccf695db716cf4bcaccd9e3a7b
Felipe Cabal <phelipekabbal_85@hotmail.com>
2024-01-26 22:41:25 -0500

    tema asincronos y sincronos

--- 3.fs-readFile.js
+++ 3.fs-readFile.js
@@ -1,9 +1,15 @@
 const fs = require('node:fs')
+const { text } = require('stream/consumers')
 
 console.log('Leyendo el primer archivo ....')
-const text = fs.readFileSync('./archivo.txt', 'utf-8')
-console.log(text)
+fs.readFile('./archivo.txt', 'utf-8', (err,text) =>(
+    console.log('Primer texto: ',text)
+) )
+
+console.log('Hacer cosas mientras lee el archivo ....')
 
 console.log('Leyendo el segundo archivo ....')
-const text2 = fs.readFileSync('./archivo2.txt', 'utf-8')
-console.log(text2)
+fs.readFile('./archivo2.txt', 'utf-8', (err,text2) => (
+    console.log('Segundo texto: ',text2)
+))
+
